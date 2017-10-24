20b4f9d87a52f15b98c498370284219fe6b6860e
ut0729 <fasolato@omicronconsulting.it>
2017-10-24 15:16:35 +0200

    Aggiunta l'applicazione console vuota

---
+++ src/main/java/net/fasolato/mina/minatest/MinaTestApplication.java
@@ -0,0 +1,18 @@
+package net.fasolato.mina.minatest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinaTestApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinaTestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+	}
+}
