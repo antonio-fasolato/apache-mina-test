9c35692d21fe760c9a3b59095f9e6fa62ba9408c
ut0729 <fasolato@omicronconsulting.it>
2017-10-24 15:20:20 +0200

    Logging

--- src/main/java/net/fasolato/mina/minatest/MinaTestApplication.java
+++ src/main/java/net/fasolato/mina/minatest/MinaTestApplication.java
@@ -1,18 +1,25 @@
 package net.fasolato.mina.minatest;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinaTestApplication implements CommandLineRunner {
+    private static Logger log = LogManager.getLogger(MinaTestApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(MinaTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MinaTestApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-
-	}
+    @Override
+    public void run(String... strings) throws Exception {
+        try {
+            log.info("Application started");
+        } finally {
+            log.info("Application stopped");
+        }
+    }
 }
